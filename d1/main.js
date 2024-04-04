37d2025743a9dc1314270a7d4707f355f4f79c49
Syahril Fitrawan Abadi <syahrilfitrawanabadi@gmail.com>
2024-04-04 21:16:44 +0800

    first commit

---
+++ d1/main.js
@@ -0,0 +1,48 @@
+// alert("Ini berfungsi")
+// console.log("berfungsi");
+// prompt('Masukkan usia Anda: ');
+
+// let usia = prompt('Berapa usia Anda?');
+// alert('Usia Anda adalah ' + usia);
+
+// let nama = 'syahril';
+// let usia = 30;
+// let tinggiBadan = 160.5;
+// let beratBadan;
+// let pacar = 3;
+
+// beratBadan = 57.4;
+
+// if (pacar == null) {
+//     pacar = 'belum punya'
+// } else {
+//     pacar = 'udah punya'
+// }
+
+// let saldoAwal = 50000
+// let saldoTambahan = 80000
+// const hutang = 30000
+// const saldoAkhir = saldoAwal + saldoTambahan - hutang
+
+// alert(`nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah 
+// ${tinggiBadan} berat badan saya ${beratBadan} dan saya ${pacar}`);
+
+// alert(
+//     `saldo awal saya sebesar Rp.${saldoAwal} & saldo tambahan yang akan saya miliki sebesar Rp.${saldoTambahan}. Jadi total saldo yang akan saya miliki adalah sebanyak Rp.${saldoAkhir}`
+// )
+
+// ARRAY
+// let namaGuru = ['jodi', 'bunga', 'rahman']
+// namaGuru.push('dea', 'fikri')
+// namaGuru.shift() // menghapus nilai pertama
+// namaGuru.pop() // menghapus nilai akhir
+// alert(namaGuru)
+
+// TUGAS KECIL
+
+// 1. Prompt untuk mengetahui saldo akhir dari apa yang diinputkan oleh user 
+// 2. Menentukan harid dari tanggal yang ada saat ini di pc kalian
+
+let hari = new Date().getDay()
+alert(`hari ini adalah hari ${hari}`)
+
