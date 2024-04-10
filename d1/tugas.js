16932774d44b8b3a6f3f12b43581e6823d227ee7
Syahril Fitrawan Abadi <syahrilfitrawanabadi@gmail.com>
2024-04-10 16:32:35 +0800

    add tugas.js pertama dari mas dea

--- d1/tugas.js
+++ d1/tugas.js
@@ -1,20 +1,19 @@
 // TUGAS KECIL
-
 // 1. Prompt untuk mengetahui saldo akhir dari apa yang diinputkan oleh user 
 // 2. Menentukan harid dari tanggal yang ada saat ini di pc kalian
 
 //Tugas 1
-let saldoAwal = 100
-let pendapatan = 500
-const thr = 50
+// let saldoAwal = 100
+// let pendapatan = 500
+// const thr = 50
 
-let saldoAkhir = saldoAwal + pendapatan + thr
-let saldo = prompt(`Saldo awal Anda adalah Rp.${saldoAwal}, dan pendapatan Anda sebelumnya Rp.${pendapatan}, kemarin anda mendapatkan THR sebesar Rp.${thr}, apakah Anda ingin melihat saldo akhir Anda dari semua yang sudah Anda dapatkan?`)
-if (saldo == "iya" || saldo == "ya") {
-   alert(`Saldo akhir Anda: Rp.${saldoAkhir}`)
-} else {
-   alert(`yaudah saya ambil nih`)
-}
+// let saldoAkhir = saldoAwal + pendapatan + thr
+// let saldo = prompt(`Saldo awal Anda adalah Rp.${saldoAwal}, dan pendapatan Anda sebelumnya Rp.${pendapatan}, kemarin anda mendapatkan THR sebesar Rp.${thr}, apakah Anda ingin melihat saldo akhir Anda dari semua yang sudah Anda dapatkan?`)
+// if (saldo == "iya" || saldo == "ya") {
+//    alert(`Saldo akhir Anda: Rp.${saldoAkhir}`)
+// } else {
+//    alert(`yaudah saya ambil nih`)
+// }
 
 // Tugas 2
 let hari = new Date().getDay()
